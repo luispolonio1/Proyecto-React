5f8d29eedf10cc605b5fc03c075c6f1e89d91f22
unknown <lpoloniol@unemi.edu.ec>
2024-06-19 17:11:21 -0500

    Modificacion Carpetas

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDom from "react-dom/client";
-import { Greeting ,User} from "./Greeting";
-import {TaksC} from "./taks"
 const root = ReactDom.createRoot(document.getElementById("root"))
 
 root.render(<>
