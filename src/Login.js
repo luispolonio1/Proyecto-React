a9834feffd1ff097d9d97eea0a190b84a48c4839
unknown <sperezs4@unemi.edu.ec>
2024-06-25 15:07:21 -0500

    Steven Perez

---
+++ src/Login.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+
+    };
+
+    return (
+        <div className="container mt-5">
+            <div className="row justify-content-center">
+                <div className="col-md-6">
+                    <h2>Login</h2>
+                    <form onSubmit={handleSubmit}>
+                        <div className="mb-3">
+                            <label htmlFor="username" className="form-label">Usuario</label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="username"
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}
+                            />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="password" className="form-label">Contraseña</label>
+                            <input
+                                type="password"
+                                className="form-control"
+                                id="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </div>
+                        <button type="submit" className="btn btn-primary" href="./index.html">Iniciar</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,19 @@ import ReactDom from "react-dom/client";
 import {BarraNavegacion,Cuerpo,Formulario} from "./Home"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import Login from "./Login";
 const root = ReactDom.createRoot(document.getElementById("root"))
+const rootL= ReactDom.createRoot(document.getElementById("rootL"))
 
 root.render(<>
 <BarraNavegacion name='Luis'/>
 <Cuerpo/>
+
 </>
 )
+
+rootL.render(<>
+<Login/>
+
+</>)
 
