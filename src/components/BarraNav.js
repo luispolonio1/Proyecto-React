41366b57a1c7a7d8acf7cb54bfcc8f8ce4758444
unknown <lpoloniol@unemi.edu.ec>
2024-06-28 17:00:30 -0500

    28/06/2024

--- src/components/BarraNav.js
+++ src/components/BarraNav.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-
+import '../estilos.css'
 export const BarraNavegacion = ({ name }) => {
   return (
     <Navbar expand="lg" className="body-tertiary sticky-top" bg='dark' data-bs-theme="dark">
@@ -17,9 +17,9 @@ export const BarraNavegacion = ({ name }) => {
             style={{ maxHeight: '100px' }}
             navbarScroll
           >
-            <NavLink to="Deportes" className="nav-link">Deportes</NavLink>
-            <NavLink to="tecnologia" className="nav-link">Tecnologia</NavLink>
-            <NavLink to="noticias" className="nav-link">Noticias Mundiales</NavLink>
+            <NavLink to="Deportes" className="nav-link borderHover">Deportes</NavLink>
+            <NavLink to="tecnologia" className="nav-link borderHover">Tecnologia</NavLink>
+            <NavLink to="noticias" className="nav-link borderHover">Noticias Mundiales</NavLink>
           </Nav>
           <Nav>
             <Nav.Link href='#Usuario' style={{ marginRight: '10px',color:'white',fontSize:'25px'}}><i className="bi bi-person" style={{ fontSize: '30px' }}></i>{name ? name : 'Usuario'}</Nav.Link>
--- src/components/Deportes.js
+++ src/components/Deportes.js
@@ -3,24 +3,31 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { ContextoBlog } from './contenido';  
 
 const Deporte = () => {
-const { blogs } = useContext(ContextoBlog)
-const deportesBlogs = blogs.filter(blog => blog.Categoria == 'Deportes')
-return (
-  <div className="container-fluid">
-      <div className="row row-cols-1 row-cols-md-4 g-4">
-        {deportesBlogs.map((blog, index) => (
-          <div className="col" key={index}>
-            <div className="card">
-              <img src={blog.Imagen} className="card-img-top" alt={blog.Titulo} style={{height:'250px'}}/>
-              <div className="card-body">
-                <h4 className="card-title">{blog.Titulo}</h4>
+  const { blogs } = useContext(ContextoBlog);
+  const deportesBlogs = blogs.filter(blog => blog.Categoria === 'Deportes');
+
+  return (
+    <div className="container-fluid mt-5">
+      {deportesBlogs.length === 0 ? (
+        <div className="text-center" style={{height:'600px',display:'flex',justifyContent:'center',alignItems:'center'}}>
+          <h1 className="display-1"style={{color:'#555'}} >Aún está vacío</h1>
+        </div>
+      ) : (
+        <div className="row row-cols-1 row-cols-md-4 g-4">
+          {deportesBlogs.map((blog, index) => (
+            <div className="col" key={index}>
+              <div className="card">
+                <img src={blog.Imagen} className="card-img-top" alt={blog.Titulo} style={{height: '250px'}} />
+                <div className="card-body">
+                  <h4 className="card-title" style={{color:'black'}}>{blog.Titulo}</h4>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      )}
     </div>
-   );
+  );
 };
 
 export default Deporte;
